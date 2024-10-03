f839a4a15d4d99401aceda9262d1f351d029b908
lucas Ferreira <lucaslfr00@gmail.com>
2024-10-02 23:57:42 -0300

    sistema de folgas e feriados esta incorreto

--- gerador_de_escala.py
+++ gerador_de_escala.py
@@ -11,8 +11,10 @@ def type_colunas(df):
     df["ultima_escala_vermelha"] = pd.to_datetime(df["ultima_escala_vermelha"])
     return df
 
-def gerarEscala(data_inicio, quantidade_de_dias, quantidade_sentinelas, feridos=None, folga=None):
+def gerarEscala(data_inicio, quantidade_de_dias, quantidade_sentinelas, feriados=[], folgas=[]):
     data_convertida = datetime.strptime(data_inicio, "%Y-%m-%d")
+    feriados = [datetime.strptime(data, "%Y-%m-%d") for data in feriados]
+    folgas = [datetime.strptime(data, "%Y-%m-%d") for data in folgas]
     df_escala_dict = []
     df_atiradores = pd.read_csv('./atiradores.csv', header=0)
     df_monitores = pd.read_csv('./monitores.csv', header=0)
@@ -27,7 +29,7 @@ def gerarEscala(data_inicio, quantidade_de_dias, quantidade_sentinelas, feridos=
     for i in range(quantidade_de_dias):
         data_da_escala = data_convertida + timedelta(days=i)
 
-        if data_da_escala.weekday() < 5:
+        if (data_da_escala.weekday() < 5) & (data_da_escala not in feriados) & (data_da_escala not in folgas):
             escala_do_dia = {'data':data_da_escala.strftime("%Y-%m-%d")}
             dia_anterior = data_da_escala + timedelta(days=-1)
             
@@ -38,8 +40,6 @@ def gerarEscala(data_inicio, quantidade_de_dias, quantidade_sentinelas, feridos=
                 disponibilidade = ast.literal_eval(df_monitores.loc[comandante,'disponibilidade'])
                 ultimo_servico_preta = df_monitores.loc[comandante,'ultima_escala_preta']
                 ultimo_servico_vermelha = df_monitores.loc[comandante,'ultima_escala_vermelha']
-                if(data_da_escala.weekday() == 1):
-                    print((data_da_escala.weekday() in disponibilidade) & (ultimo_servico_preta != dia_anterior) & (ultimo_servico_vermelha != dia_anterior))
 
                 if (data_da_escala.weekday() in disponibilidade) & (ultimo_servico_preta != dia_anterior) & (ultimo_servico_vermelha != dia_anterior):
                     nome = df_monitores.loc[comandante,'nome']
@@ -49,7 +49,6 @@ def gerarEscala(data_inicio, quantidade_de_dias, quantidade_sentinelas, feridos=
             else:
                 print(f"não foi possivel gerar uma escala para o dia {escala_do_dia}")
                 escala_do_dia['comandante'] = f"não foi possivel gerar uma escala para o dia {escala_do_dia}"
-
 
             n_sent = 1
             for qtd in range(quantidade_sentinelas):
@@ -70,10 +69,8 @@ def gerarEscala(data_inicio, quantidade_de_dias, quantidade_sentinelas, feridos=
                     print(f"não foi possivel gerar uma escala para o dia {escala_do_dia}")
                     escala_do_dia = {'data':data_da_escala.strftime("%Y-%m-%d")}
                     escala_do_dia['comandante'] = f"não foi possivel gerar uma escala para o dia {escala_do_dia}"
-            
-            df_escala_dict.append(escala_do_dia)
 
-        else:
+        elif ((data_da_escala.weekday() >= 5) | (data_da_escala in feriados)) & (data_da_escala not in folgas):
             escala_do_dia = {'data':data_da_escala.strftime("%Y-%m-%d")}
             dia_anterior = data_da_escala + timedelta(days=-1)
             
@@ -91,15 +88,13 @@ def gerarEscala(data_inicio, quantidade_de_dias, quantidade_sentinelas, feridos=
                     df_monitores.loc[comandante,'ultima_escala_vermelha'] = data_da_escala.strftime("%Y-%m-%d")
                     break
             else:
-                print(f"não foi possivel gerar uma escala para o dia {escala_do_dia}")
-                escala_do_dia['comandante'] = f"não foi possivel gerar uma escala para o dia {escala_do_dia}"
-
+                print(f"não foi possivel gerar uma escala para o dia {data_da_escala}")
+                escala_do_dia['comandante'] = f"não foi possivel gerar uma escala para o dia {data_da_escala}"
 
             n_sent = 1
             for qtd in range(quantidade_sentinelas):
                 for sentinela in range(len(df_atiradores)):
                     df_atiradores = ordenar_df(df_atiradores,'ultima_escala_vermelha')
-
                     disponibilidade = ast.literal_eval(df_atiradores.loc[sentinela,'disponibilidade'])
                     ultimo_servico_preta = df_atiradores.loc[sentinela,'ultima_escala_preta']
                     ultimo_servico_vermelha = df_atiradores.loc[sentinela,'ultima_escala_vermelha']
@@ -111,18 +106,17 @@ def gerarEscala(data_inicio, quantidade_de_dias, quantidade_sentinelas, feridos=
                         n_sent += 1
                         break
                 else:
-                    print(f"não foi possivel gerar uma escala para o dia {escala_do_dia}")
+                    print(f"não foi possivel gerar uma escala para o dia {data_da_escala}")
                     escala_do_dia = {'data':data_da_escala.strftime("%Y-%m-%d")}
-                    escala_do_dia['comandante'] = f"não foi possivel gerar uma escala para o dia {escala_do_dia}"
-            
-            df_escala_dict.append(escala_do_dia)
-
-    print(df_escala_dict)
+                    escala_do_dia['comandante'] = f"não foi possivel gerar uma escala para o dia {data_da_escala}"
+        else:
+            print("folga")
+        df_escala_dict.append(escala_do_dia)
 
     df_escala_finalizada = pd.DataFrame(df_escala_dict)
+    print(df_escala_dict)
 
     df_escala_finalizada.to_csv(f'./escalas/escala_servico_dia_{data_str}.csv', index=False)
-    return None
 
 if __name__ == "__main__":
-    gerarEscala("2024-09-10", 60,3)
+    gerarEscala(data_inicio="2024-10-01", quantidade_de_dias=60, quantidade_sentinelas=3, feriados=[], folgas=["2024-10-05","2024-10-04"])
